29a2687f60450e3a5a2b2d28991215dace978a93
Jeremy Carroll <jeremy@pinterest.com>
2013-07-11 21:38:37 +0000

    Added ldconfig to build_jzmq from mbautin fork

--- build_jzmq.sh
+++ build_jzmq.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -x
 name=libjzmq
-arch='amd64' # Change to your architecture
 version=2.1.7
+description="JZMQ is the Java bindings for ZeroMQ"
 url='https://github.com/nathanmarz/jzmq.git'
-buildroot=build
-fakeroot=jzmq
-origdir=$(pwd)
+arch="$(dpkg --print-architecture)"
+section="misc"
+package_version=""
+origdir="$(pwd)"
 prefix="/usr"
-description='JZMQ is the Java bindings for ZeroMQ'
+
 # Make sure JAVA_HOME is set. Uncomment if necessary
 if [ "${JAVA_HOME}x" == "x" ]; then
   echo Please set JAVA_HOME before running.
@@ -16,25 +17,46 @@ if [ "${JAVA_HOME}x" == "x" ]; then
 fi
 
 #_ MAIN _#
+# Cleanup old debian files
 rm -rf ${name}*.deb
-rm -rf jzmq
-rm -rf ${buildroot}
-mkdir -p ${buildroot}
+# If temp directory exists, remove if
+if [ -d tmp ]; then
+  rm -rf tmp
+fi
+# Make build directory, save location
+mkdir -p tmp && pushd tmp
+# Clone Git repository & make build directory
 git clone ${url}
 cd jzmq
-sed -i 's/classdist_noinst.stamp/classnoinst.stamp/g' src/Makefile.am
+mkdir -p build
+# Patch for 12.x releases
+if [ $(cat /etc/lsb-release|grep -i release|grep 12\.) ]; then
+  curl -vs https://github.com/nathanmarz/jzmq/pull/2.patch 2>/dev/null | patch -p
+fi
+# Build package
 ./autogen.sh
 ./configure --prefix=${prefix}
-# https://github.com/zeromq/jzmq/issues/114#issuecomment-6927797
-touch src/classdist_noinst.stamp
-cd src/
-CLASSPATH=.:./.:$CLASSPATH
-javac -d . org/zeromq/ZMQ.java org/zeromq/App.java org/zeromq/ZMQForwarder.java org/zeromq/EmbeddedLibraryTools.java org/zeromq/ZMQQueue.java org/zeromq/ZMQStreamer.java org/zeromq/ZMQException.java
-cd ..
+#cd src/
+#CLASSPATH=.:./.:$CLASSPATH
+#javac -d . org/zeromq/ZMQ.java org/zeromq/App.java org/zeromq/ZMQForwarder.java org/zeromq/EmbeddedLibraryTools.java org/zeromq/ZMQQueue.java org/zeromq/ZMQStreamer.java org/zeromq/ZMQException.java
+#cd ..
 make
-make install DESTDIR=${origdir}/${buildroot}
-
+make install DESTDIR=`pwd`/build
 #_ MAKE DEBIAN _#
-cd ${origdir}/${buildroot}
-fpm -t deb -n ${name} -v ${version} --description "${description}" --url="${url}" -a ${arch} --prefix=/ -d "libzmq0 >= 2.1.7" -s dir -- .
-mv ${origdir}/${buildroot}/*.deb ${origdir}
+cd build
+fpm -t deb \
+    -n ${name} \
+    -v ${version}${package_version} \
+    --description "${description}" \
+    --url="${url}" \
+    -a ${arch} \
+    --category ${section} \
+    -m "${USER}@localhost" \
+    --prefix=/ \
+    -d "libzmq0 >= 2.1.7" \
+    -s dir \
+    --after-install ${origdir}/shlib.postinst \
+    --after-remove ${origdir}/shlib.postuninst \
+    -- .
+mv ${name}*.deb ${origdir}
+popd
